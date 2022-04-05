a05a0b2fae25d3c8b02431e388b083b2d6d1a74e
CarolinaMartine5 <86535492+CarolinaMartine5@users.noreply.github.com>
2022-04-05 00:09:05 -0400

    Updated
    This included a portion of the Map, Introduction and Checklist Requirement

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import streamlit as st
+import yfinance as yf
 import requests
 import config
 
-st.title("The Stock Market")
+#Changes the Favicon and Tab Title
+st.set_page_config(
+    page_title="Stock Dashboard",
+    page_icon="chart_with_upwards_trend",
+    layout="wide",
+)
+st.title("Stock Market Dashboard")
 
-stock_ticker= st.text_input("Enter a valid stock tiker....AAPL")
+#Input from the user in order to get a Stock
+stock_ticker= st.text_input("Enter a valid stock ticker....AAPL")
 api_token= config.stockData_api_key
+stockYahoo = yf.Ticker(stock_ticker)
 
 if stock_ticker:
     parameters = {
@@ -17,7 +26,28 @@ if stock_ticker:
 
     st.write(stockData)
 else:
-    st.warning("empty value")
+    st.warning("Please input a Stock's ticker")
 
+information_block = st.checkbox("See an Overview of the Company")
+if information_block:
+    st.info(stockYahoo.info['longBusinessSummary'])
+
+headquarter_map = st.checkbox("See the Company's Headquarter")
+if headquarter_map:
+    company_name = yf.Ticker(stock_ticker).info['longName']
+    st.write(company_name)
+
+#Introduction to the Map Class
+def map_creator(latitude,longitude):
+    from streamlit_folium import folium_static
+    import folium
+    # center on the station
+    m = folium.Map(location=[latitude, longitude], zoom_start=10)
+
+    # add marker for the station
+    folium.Marker([latitude, longitude], popup="Station", tooltip="Station").add_to(m)
+
+    # call to render Folium map in Streamlit
+    folium_static(m)
 
 
