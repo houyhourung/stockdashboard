2bd6271c8a58ec93de702ee5860d5ac9879458e5
CarolinaMartine5 <86535492+CarolinaMartine5@users.noreply.github.com>
2022-04-05 23:57:59 -0400

    Separation of Information Class and Map - Deleted Map Interface class

--- app.py
+++ app.py
@@ -18,17 +18,20 @@ stock_ticker=  st.sidebar.text_input("Enter a valid stock ticker....AAPL")
 api_token= config.stockData_api_key
 stockYahoo = yf.Ticker(stock_ticker)
 
+# This gives an overview of the company - Information Box Widget
 def information():
-
-    # This gives an overview of the company - Information Box Widget
     information_block = st.sidebar.checkbox("See an Overview of the Company")
     if information_block:
         st.info(stockYahoo.info['longBusinessSummary'])
+    pass
 
+# Map class
+def map():
     headquarter_map = st.sidebar.checkbox("See the Company's Headquarter")
     if headquarter_map:
         address = (stockYahoo.info['address1'], stockYahoo.info['city'])
         st.write(address)
+
     pass
 
 
@@ -43,20 +46,7 @@ if stock_ticker:
     st.title(stockData["data"][0]["name"] + "'s Dashboard")
     st.write(stockData)
     information()
+    map()
 else:
     st.warning("Please input a Stock's ticker")
-
-#Introduction to the Map Class
-def map_creator(latitude,longitude):
-    from streamlit_folium import folium_static
-    import folium
-    # center on the station
-    m = folium.Map(location=[latitude, longitude], zoom_start=10)
-
-    # add marker for the station
-    folium.Marker([latitude, longitude], popup="Station", tooltip="Station").add_to(m)
-
-    # call to render Folium map in Streamlit
-    folium_static(m)
-
 
