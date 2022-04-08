3b7ecc18945073beaf494c40bdc7454fa5a58256
CarolinaMartine5 <86535492+CarolinaMartine5@users.noreply.github.com>
2022-04-07 23:19:15 -0400

    Changed what the Icon on the Map says and fixed formatting

--- app.py
+++ app.py
@@ -55,11 +55,9 @@ def map_creator(latitude,longitude):
     from streamlit_folium import folium_static
     import folium
     # center on the station
-    m = folium.Map(location=[latitude, longitude], zoom_start=10)
-
+    m = folium.Map(location=[latitude, longitude], zoom_start=12)
     # add marker for the station
-    folium.Marker([latitude, longitude], popup="Station", tooltip="Station").add_to(m)
-
+    folium.Marker([latitude, longitude], popup="HeadQuarters", tooltip="HeadQuarters").add_to(m)
     # call to render Folium map in Streamlit
     folium_static(m)
 
